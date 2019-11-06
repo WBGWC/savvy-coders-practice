eec543b3ae6024abbfe35d06133914d41356ecfe
Wallat Baban <WlatM.Baban@gmail.com>
2019-11-05 21:06:08 -0600

    Example JS coding

---
+++ index.js
@@ -0,0 +1,19 @@
+const myName = "Wallat Baban";
+
+// Template Literal
+console.log(`Hello World! this is ${myName}`);
+
+const me = {
+  name: "Wallat",
+  age: 22,
+  isTruth: true,
+  maxIq: 160,
+  weight: 190
+
+};
+
+//We are using Bracket Notation
+console.log(me["name"]);
+
+//This is Dot Notation
+console.log(me.age);
