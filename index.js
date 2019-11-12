5688c9068a1c104caab26317f5b0ba66c7184e4f
Wallat Baban <WlatM.Baban@gmail.com>
2019-11-12 17:30:04 -0600

    New Class Notes

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+
+// Week 2, Day 1
 const myName = "Wallat Baban";
 
 // Template Literal
@@ -29,6 +31,7 @@ console.log(me.age);
  *
  */
 
+ // Day 2 - Week 3
 const robot = {
 
   name = "Wally the Robot"
@@ -38,6 +41,7 @@ const robot = {
     organizing: true
   };
 
+  // This is a method below
     checkJobCompletion: function() {
       //The bottom is a Getter
       return this.job.cleaning === true;
@@ -104,4 +108,88 @@ function numberSummer(startingValue = 1, endingValue = 10) {
     return currentTotal;
   }
   //We invoke Functions and can pass in arguments that 'match up' to the named parameters
-  console.log(numberSummer (1, 10));
+  console.log(numberSummer ());
+
+/*
+ * Attempted Code for HW3 ******************
+ * const numberSummerMachine = {
+ *   startingNumber: 0,
+ *    endingNumber: 23,
+ * }
+ *  numberSummerMachine.sumDaNumbers = function(startingNumber, endingNumber){
+ *   while(startingNumber <= 23) {
+ *      endingNumber += startingNumber;
+ *      startingNumber += 1;
+ *    }
+ * }
+ * console.log(endingNumber);
+ */
+
+
+//THE CODE BELOW IS FROM MANAV FOR HW3
+const me = {
+  name: "manav"
+  name: 'mark',
+}		   age: 3,
+  homework: {
+    hw1: "passed",
+    hw2: "failed",
+  },
+  // Method
+  checkDrinkingAge: function() {
+    // Functions should have an EXPLICIT RETURN
+    // Comparison operator - always return boolean
+    return this.age >= 21;
+  },
+  checkHomework1: function() {
+    // Getter
+    return this.homework.hw1;
+  }
+};
+
+ // Chaining
+me.homework.hw2 = 'passed';
+
+ // console.log(me.checkDrinkingAge());
+
+ // COERCION
+let x = 32;
+let y = false;
+
+//The bottom notations are for crossing something out.
+ // // JS defaults to coercing numbers into Strings
+
+// console.log(x === y);
+ //Concatenation and coercion
+console.log(x + Number(y));
+
+
+// THESE ARE NEW NOTES FROM HEATHER NOTES*****
+// create a loop that starts at 1 and ends at 10.
+//keep track of the current count.
+// keep track of the total
+let currentTotal = 0;
+let currentNum = 0;
+//expression that evaluates to a boolean
+while (currentNum <= 10) {
+    currentTotal += currentNum;
+    currentNum += 1; //currentNum = currentNum + 1;
+}
+console.log(currentTotal);
+//refactoring - rewriting the code for better readability or performance w/o affecting the functionality
+let newTotal = 0;
+//initializer; exit condition; iterator
+for (let i = 1; i <= 10; i += 1) {
+    newTotal += i; //newTotal = newTotal +i
+}
+console.log(newTotal);
+//add function
+// named parameters allow functions to receive additional information to complete a task
+function numberSummer(startingValue = 0, endingValue = 10) {
+    let otherTotal = 0;
+    for (let i = startingValue; i <= endingValue; i += 1) {
+        otherTotal += i;
+    }
+    return otherTotal;
+} //we invoke functions and can pass in arguments that 'match up' to the named parameters
+console.log(numberSummer());
