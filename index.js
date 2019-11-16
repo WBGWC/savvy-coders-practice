8307df9840b981b3bd668f72dbf78d17ac9ee734
Wallat Baban <WlatM.Baban@gmail.com>
2019-11-15 19:21:03 -0600

    Adding in 11/15/2019 JS  update

--- index.js
+++ index.js
@@ -1,19 +1,15 @@
-//**This if Fizz Buzz
-//* If we pass a number that is divisible by 5 and 3, log 'fizzBuzz'
-//* if only divisible by 3, log 'fizz'
-//* if only divisible by 5, log 'buzz'
-//* Loop over the numbers from 1 to 99
-
-function fizzBuzz(number){
-  for (let i = 1; i <= 100, i += 1) {
-    if(i % 3 === 0 && i % 5 ===0) {
-      console.log('FizzBuzz');
-    } else if (i % 5 === 0) {
-      console.log('buzz');
-    } else if (i % 3 === 0) {
-      console.log('fizz');
-    } else {
-      console.log(i);
-      }
+//* 11.15.2019
+function initPerson(fname, lname, age) {
+  return {
+    fname,
+    lname,
+    age,
+    canDrink() {
+      age >= 21;
     }
-  }
+  };
+}
+
+const me = initPerson("manav", "misra", 13);
+console.log(me);
+console.log(me.canDrink());
