4805fcf3fd862b2ed8d9d5e94f4eef41c227fa4e
Wallat Baban <WlatM.Baban@gmail.com>
2019-11-15 19:05:23 -0600

    Add more and more JS stuff

--- index.js
+++ index.js
@@ -1,115 +1,30 @@
-// This is called Encapsulation and for a While Loop
-const numberSummerMachine = {
-  startingNumber: 1,
-  endingNumber: 123,
-  sumDaNumbers: function() {
-    let total  = 0;
-    let i = this.startingNumber;
+//Moduleas operator = %
+// Comparison Operators: ===, >=, <=
 
-    while (i <= this.endingNumber) {
-      total += i;
-      i += 1;
-    }
-    return total;
-  },
-  // This setter allows for updating of the 'local data'
-  //the bottom is a function without saying : function
-  setStartingNumber(num) {
-    this.startingNumber = num;
+
+// Logical Operators are these:
+const x = 3;
+const y = 5;
+const z = 10;
+
+//Logical operators involve AND and OR conditions
+// And operator checks both the left and right operands must be true
+console.log(x < y && x < z);
+
+// Or operator is if the left operator is true, right is never evaluated.
+// || allows us to do short circuiting;
+console.log(x < y || x < z);
+
+//*Example of old way to short circuit.
+/*function numberSummer(start, end);
+/*let s = start || 1;
+*/
+
+function  highOrLow(x, y) {
+  if (x > y) {
+    return "x is more";
   }
-};
-//This makes the starting number 5
-numberSummerMachine.setStartingNumber(5);
-
-console.log(numberSummerMachine.sumDaNumbers());
-
-// Learning About Functions
-function doMath(x ,y, mathFxn) {
-  console.trace();
-  return mathFxn(x, y);
+//The below is called Short Circuiting :D
+  return "y is more";
 }
-
-function add (x, y) {
-  console.trace();
-  return x + y;
-}
-
-console.log(doMath(add));
-
-// OR for subtraction
-
-function doMath(x, y, mathFxn) {
-  console.trace();
-  // the function will receive x and y
-  return mathFxn(x, y);
-}
-
-function subtract(x, y) {
-  console.trace();
-  return x - y;
-}
-console.log(doMath(5, 10, subtract));
-
-
- //Another Example, however this is a function that returns another function.
-//*This is a Function Factory that returns another function
-//This Function Factory uses the concept of a closure.
-
-/**
-*  * initSportScorer is the OUTER Function
- * it receives pts, but then immediately discards its references to pts.
- * pts remains referenced by the INNER FUNCTION -  this is an example of a CLOSURE
- * a variable loses reference from the original function but has become 'enclosed' with a reference to the INNER FUNCTION
- */
-function initSportsScorer(pts) {
-  return function(score) {
-    return score + pts;
- }
-}
-const threePointer = initSportsScorer(3);
-
-const touchdown = initSportsScorer(21);
-//**
-// * function (score) {
-// * return score + 3;
-//* }
-//*/
-
-console.log(threePointer(99));
-console.log(touchdown(7))
-
-
-
-//* This bottom function is to return an Error
-function foo() {
-  console.trace();
-  bar();
-  console.trace();
-
-  return "foo";
-}
-
-function bar() {
-  console.trace();
-  baz();
-  console.trace();
-
-  return "bar";
-}
-
-function baz() {
-  console.trace();
-
-  return "baz";
-}
-
-console.log(foo());
-
-
-// A and B are name parameters in this Function Declaration
-function myFunc(a, b) {
-  console.log(a, b);
-  return "hello" + a + b;
-}
-// 2 and 3 are arguments that are passed upon function invocation
-myFunc(2, 3);
+console.log(highOrLow(3, 5));
