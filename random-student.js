60d8077c6d896682aeb12fdd8ad8bbd92e1a9532
Wallat Baban <WlatM.Baban@gmail.com>
2019-11-15 21:19:25 -0600

    Add final commit to random student generator

--- random-student.js
+++ random-student.js
@@ -1,12 +1,34 @@
-const myArr = [1, "man", true, null]; //* can put any value in this array
+const students = [1, "man", true, null]; //* can put any value in this array
 
-const myArr = [["Andrew","Billy","George","Heather","Madison","Marque","Martin","Nikki","Oscar","RhondaLyn","Sidney","Tracie","Wallat"]]
+const students = [["Andrew","Billy","George","Heather","Madison","Marque","Martin","Nikki","Oscar","RhondaLyn","Sidney","Tracie","Wallat"]]
 
 console.log(myArr[0].length) //This is for when an array is nested like the above to access the length of the inner array.
 
-const myArr = ["Andrew","Billy","George","Heather","Madison","Marque","Martin","Nikki","Oscar","RhondaLyn","Sidney","Tracie","Wallat"]
+
+//* The application we are making ************************************
+const students = ["Andrew","Billy","George","Heather","Madison","Marque","Martin","Nikki","Oscar","RhondaLyn","Sidney","Tracie","Wallat"]
 
 
+// Used the URL: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+}
+
+console.log(students[getRandomInt(0, students.length - 1)]);
+
+//* End of Web Application ******************************************
+
+
+
+
+
+
+console.log(Math.floor(Math.random() * 1000))
+
+Math.floor() //* rounds down
+Math.ceiling() //* rounds up
 
 const myObj = {};
 
