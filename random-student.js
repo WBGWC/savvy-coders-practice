b551823a09a7479123ee8dc6dfb22a397a63b03d
Wallat Baban <WlatM.Baban@gmail.com>
2019-11-15 21:06:11 -0600

    Add Array of Students

---
+++ random-student.js
@@ -0,0 +1,17 @@
+const myArr = [1, "man", true, null]; //* can put any value in this array
+
+const myArr = [["Andrew","Billy","George","Heather","Madison","Marque","Martin","Nikki","Oscar","RhondaLyn","Sidney","Tracie","Wallat"]]
+
+console.log(myArr[0].length) //This is for when an array is nested like the above to access the length of the inner array.
+
+const myArr = ["Andrew","Billy","George","Heather","Madison","Marque","Martin","Nikki","Oscar","RhondaLyn","Sidney","Tracie","Wallat"]
+
+
+
+const myObj = {};
+
+myObj.name = "Manav";
+
+myObj["age"] = 34; //* This is a bracket notation and can use this to reference it.
+
+myArr[0] = "hello";
