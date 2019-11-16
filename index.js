127f8e75ec3e289ed1be29259b40a96174e5737c
Wallat Baban <WlatM.Baban@gmail.com>
2019-11-15 19:10:40 -0600

    Add further JS info

--- index.js
+++ index.js
@@ -1,32 +1,25 @@
-//*Exponential
+//* Factorial - num * num - 1 * num - 2...all the way to num === 0
+// 4 * 3 * 2 * 1:
 
-//* This is a function returning 8 using a For loop.
-function myPow(num, exp) {
-  let ret = num
-  for (let i = 1; i < exp; i += 1) {
-    ret *= num;
+//*This one is recursion solution
+function factorial(num) {
+  if(num === 0) {
+    return 1;
+  }
+
+  return num * factorial(num - 1);
+}
+
+console.log(factorial(4));
+
+
+//* this is For loop solution
+function factorial(num) {
+  let ret = 1;
+  for (let i = num i >= 1; i -= 1) {
+    ret += i;
   }
   return ret;
 }
 
-console.log(myPow(2, 3));
-
- //* The below is a recursion returning 8
-function myPow(num, exp) {
-  if(ret === 2) {
-    ret *= num;
-  }
-  return `${myPow(2, 3)}`;
-}
-console.log(myPow(2, 3));
-
-//* Correct Recursion Solution
-
-function myPow(num, exp) {
-  if (exp === 0) {
-    return 1;
-  }
-  return (num *= myPow(num, exp - 1));
-}
-
-console.log(myPow(2, 3));
+console.log(factorial(4));
--- index2.js
+++ index2.js
@@ -1,29 +1,3 @@
-//* Factorial - num * num - 1 * num - 2...all the way to num === 0
-// 4 * 3 * 2 * 1:
-
-//*This one is recursion solution
-function factorial(num) {
-  if(num === 0) {
-    return 1;
-  }
-
-  return num * factorial(num - 1);
-}
-
-console.log(factorial(4));
-
-
-//* this is For loop solution
-function factorial(num) {
-  let ret = 1;
-  for (let i = num i >= 1; i -= 1) {
-    ret += i;
-  }
-  return ret;
-}
-
-console.log(factorial(4));
-
 //* Pass by reference and pass by value
 
 
