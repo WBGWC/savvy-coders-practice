f7644007450326ebbc37cc5e1fd8985bdd6c0b16
Wallat Baban <WlatM.Baban@gmail.com>
2019-11-15 19:11:50 -0600

    Add to the updated JS more

--- index.js
+++ index.js
@@ -1,25 +1,38 @@
-//* Factorial - num * num - 1 * num - 2...all the way to num === 0
-// 4 * 3 * 2 * 1:
+//* Pass by reference and pass by value
+//*****This is a Pass by Value example */
+let x = 3;
+let y = x;
 
-//*This one is recursion solution
-function factorial(num) {
-  if(num === 0) {
-    return 1;
-  }
-
-  return num * factorial(num - 1);
+function add1(n) {
+  return n + 1;
 }
 
-console.log(factorial(4));
+// Primitives CANNOT be mutated
+//Primitives are passed by value AKA pass by copy
 
+// x Throws away its 3 and gets a whole new value of 4;
 
-//* this is For loop solution
-function factorial(num) {
-  let ret = 1;
-  for (let i = num i >= 1; i -= 1) {
-    ret += i;
-  }
-  return ret;
+x = add1(x);
+console.log(y)
+
+//****** This is a Pass by Reference example and its where the two values are the same */
+
+//* Objects CAN BE MUTATED */
+const me = {
+  name: "Mark",
+  age: 23
 }
 
-console.log(factorial(4));
+const me2 = me;
+
+function nameChanger(obj, newName) {
+  obj.name = newName;
+}
+
+//or could use:
+
+me.name = "Fred"; // Does the same thing
+
+nameChanger(me, "Fred");
+console.log(me);
+console.log(me2);
