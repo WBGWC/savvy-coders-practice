a290bba3e57ae39ad54ca924819e30f14b38ba27
Wallat Baban <WlatM.Baban@gmail.com>
2019-11-15 18:57:36 -0600

    Add more updated JS

--- index.js
+++ index.js
@@ -1,33 +1,39 @@
-// Day 2 - Week 3
-const robot = {
-  name = "Wally the Robot",
-  age = 23,
-  job: {
-  cleaning: true,
-  organizing: true,
-},
+//While Loop
+let currentNum = 0;
+let currentTotal = 0;
 
-// This is a method below
-  checkJobCompletion: function() {
-    //The bottom is a Getter
-    return this.job.cleaning === true;
-    //The above is not necessarily useful.
+while(currentNum <= 10) {
+  currentTotal += currentNum;
+
+  currentNum += 1; //currentNum = currentNum + 1
+}
+
+console.log(currentTotal);
+
+//For loops
+//Refactoring - rewrite the code for better readability or performance w/o affecting the functionality
+
+let currentTotal = 0
+
+//Initializer; Exit Condition; Iterator
+for(let i = 1; i <= 10; i += 1) {
+  console.log('i is', i)
+  currentTotal += 1; //currentTotal = currentTotal + i;
+  console.log('currentTotal is', currentTotal);
   }
-};
-// This is Coercion
+  return currentTotal;
 
-let x = 32;
-let y = "32";
 
-//*//JS defaults to coercing numbers into strings
-////when //*// is used it crosses out the entire thing you wrong.
+//Named Parameters allow functions to receive additional information to complete a task
+//We can set Default Values for our Named Parameters
+function numberSummer(startingValue = 1, endingValue = 10) {
+  let currentTotal = 0;
 
-console.log(x == y);
-// This is to coerce for sure and not to make the two completely and definitively equal.
-
-//Adding numbers 1-10
-/*
-Create a loop that starts at 1 and ends at 10.
-Keep track of the current count.
-Keep track of the running total
-*/
+  //Initializer; Exit Condition; Iterator
+  for(let i = startingValue; i <= endingValue; i += 1) {
+    currentTotal += i; //currentTotal = currentTotal + i;
+    }
+    return currentTotal;
+  }
+  //We invoke Functions and can pass in arguments that 'match up' to the named parameters
+  console.log(numberSummer ());
--- index2.js
+++ index2.js
@@ -1,37 +1,3 @@
-// Day 2 - Week 3
-const robot = {
-    name = "Wally the Robot",
-    age = 23,
-    job: {
-    cleaning: true,
-    organizing: true,
-  },
-
-  // This is a method below
-    checkJobCompletion: function() {
-      //The bottom is a Getter
-      return this.job.cleaning === true;
-      //The above is not necessarily useful.
-    }
-};
-// This is Coercion
-
-let x = 32;
-let y = "32";
-
-//*//JS defaults to coercing numbers into strings
-////when //*// is used it crosses out the entire thing you wrong.
-
-console.log(x == y);
-// This is to coerce for sure and not to make the two completely and definitively equal.
-
-//Adding numbers 1-10
-/*
-Create a loop that starts at 1 and ends at 10.
-Keep track of the current count.
-Keep track of the running total
-*/
-
 //While Loop
 let currentNum = 0;
 let currentTotal = 0;
