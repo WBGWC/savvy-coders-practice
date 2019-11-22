94e4cad6af4b77fa8e70b9de88dfe3dbcee5968e
Wallat Baban <WlatM.Baban@gmail.com>
2019-11-21 20:15:36 -0600

    Add a proper function constructor

--- index.js
+++ index.js
@@ -1,31 +1,36 @@
+// Constructor function
+​
+// What a Constructor function does is lays out the blueprint of an object
+​
+//Person is NOT an object yet
 
-// Capital A or any letter is a convention, so we must use keyword new when using const output.
-function Animal (fname, lname, age, diet) {
-  this.fname = fname,
-  this.lname = lname,
-  this.age = age,
-  this.diet = diet;
-
-this.canEat = function() {
-  if(this.age <= 2) {
-    return "I am a talking animal and i can eat food now!";
-  } else
-    return "I still like to drink milk";
-};
-
-this.getBio = function() {
-  return `Hi! My name is ${this.getFullName()}. I am ${
-    this.age
-  } years old ${this.canEat()}`;
-};
-
-this.getFullName = function() {
-  return `${this.fname} ${this.lname}`;
-  };
-};
-
-const ani = new Animal("Brian", "Griffin", 5, "Carnivore");
-console.log(ani);
-
-
-console.log(ani.getBio())
+//Prototypes can be used to as a
+​
+function Person(name, age, job) {
+  this.name = name;
+  this.age = age;
+  this.job = job;
+}
+​
+Person.prototype.whatIsName = function() {
+  if ( typeof this.name === "string" ) {
+    return this.name
+  }
+  return "This is no name entered"
+}
+​
+Person.prototype.addHobbies = function(array) {
+  this.hobbies = array
+}
+​
+const hobbies = ["coding", "music", "video games"];
+​
+// This is a function
+// Creates an instance of the blueprint
+const me = new Person("Nick", 23, "Developer")
+​
+​
+console.log(me);
+me.whatIsName();
+me.addHobbies(hobbies[0]);
+console.log(me);
