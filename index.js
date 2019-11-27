cf52881024ebf877d5344b8620c37f6eba2d4e84
Wallat Baban <WlatM.Baban@gmail.com>
2019-11-26 21:27:19 -0600

    Add fruit demo for pop, slice, splice, push, shift, unshift,etc

--- index.js
+++ index.js
@@ -1,169 +1,69 @@
-// class User{
-// constructor(fname, lname, email, currentlvl, exptonxtlvl) {
-//   this.fname = fname;
-//   this.lname = lname;
-//   this.email = email;
-//   this.currentlvl = currentlvl;
-//   this.exptonxtlvl = exptonxtlvl;
-// }
+// const fruits = ["apple", "kiwi", "pineapple", "jackfruit"];
 
-// get bio() {
-//     return `I am Player ${this.fullName()}. `;
-//   }
+// const moreFruits = ["dragonfruit", "pineapple", "mango", "guava", "strawberry"];
 
-// get level() {
-//   if (this.currentlvl < 100) {
-//     return `Exp to next level: ${this.exptonxtlvl}`;
-//   }
-// return "You still need keep training";
-// }
+// console.log("fruits is", fruits);
+// console.log("moreFruits is", moreFruits);
 
-// get fullName() {
-//   return `Your name is ${this.fname} ${this.lname}`;
-// }
+// // concat joins arrays together
+// const allTheFruits = fruits.concat(moreFruits);
+// console.log("allTheFruits is", allTheFruits);
 
-// /**
-//    * @param {any} newLName
-//    */
-// set changeName(newLName) {
-//   if(newLName) {
-//   this.lname = newLName;
-//   } else {
-//     console.error("bad name received");
-//   }
-// }
-// }
-// const u = new User('Mike', 'Johnson', 'WBGWC@mail.umsl.edu', 18, 100)
-// u.changeName = "Matt"
-// console.log(u);
+// // concat is not mutating
+// console.log("fruits is still", fruits);
+// console.log("moreFruits is still", moreFruits);
 
-// class WarriorPlayer extends User {
-//   constructor(fname, lname, email, currentlvl, exptonxtlvl, warriorid){
-//     super(fname, lname, email, currentlvl, exptonxtlvl);
-//     this.id = warriorid;
-// };
-// get bio() {
-//   return "Welcome to the game!";
-// }
+// const popped = fruits.pop();
+// console.log(popped);
+// console.log(fruits);
 
-// get warriorid() {
-//   return `Your Player id is ${this.id}`;
-// }
-// set warriorid(id) {
-//   this.id = id;
-// }
-// }
-// const war = new WarriorPlayer("Elton", "John", "ABC@gmail.com", 1, 100, 2310);
-// war.warriorid = 2309
-// console.log(war);
-// console.log(war.bio)
-// console.log(war.warriorid)
+// const pushed = fruits.push("orange");
 
-// // //Getter
+// console.log(pushed);
+// console.log(fruits);
 
-// // Person.prototype.getFaveGames = function() {
-// //   //Just create some string here that we can update
-// //   let gamesList = `Here are the favorite games for ${this.fullName()}`;
-
-// //   for (let i = 0; i <this.favGames.length; i+= 1) {
-// //     //Just concatenate the string same as we can do with the numbers.
-// //     gameList += `$(i + 1) - ${this.favGames[i]}`;
-// //   }
-// //   return gamesList;
-// // }
-
-// // function User(fname, lname, currentlvl, exptonxtlvl) {
-// //   this.fname = fname,
-// //   this.lname = lname,
-// //   this.currentlvl = currentlvl,
-// //   this.exptonxtlvl = exptonxtlvl;
-
-// // }
-
-// // User.prototype.setSkills = function(newSkill) {
-
-// //   this.userSkills = newSkill
-
-// // };
-
-// // User.prototype.getSkills = function() {
-// //   let skillsList = `Here are my skills `
-// //   for(let i = 0; i < this.userSkills.length; i+=1){
-// //     skillsList += `${this.userSkills [i]}
-// //     `;
-// //   }
-// //   return skillsList
-// // }
-
-// // // this.canLvl = function() {
-// // //   if(this.currentlvl <= 100) {
-// // //     return `Exp to next level: ${this.exptonxtlvl}`;
-// // //   }
-
-// // // this.getFullName = function() {
-// // //   return `${this.fname} ${this.lname}`;
-// // //   };
-// // // }
-
-// // const pla = new User("WBGWC", "Asin", 10,  1218);
-// // console.log(pla);
-// // const skills = ["highjump", "roll"]
-// // pla.setSkills(skills)
-// // console.log(pla);
-// // console.log(pla.getSkills());
-
-// // //console.log(pla.getBio())
-
-const numbers = [1, 2, 3, 4, 5, 6];
-const strings = [
-  "hello",
-  "world",
-  "It ya boy Tyrone, coming at you live from LA."
+const fruits = [
+  "apple",
+  "kiwi",
+  "pineapple",
+  "jackfruit", // index is 3
+  "dragonfruit",
+  "pineapple",
+  "mango",
+  "guava", // index is 7
+  "strawberry"
 ];
-//concat is ideal b/c it doesn't mutate the original data
-const numbersAndStrings = numbers.concat(strings);
-//forEach needs to know what it should do for each element - callback fxn
-//number is similar to doing numbers[i] in our traditional for loop
 
-//Arrow Syntax DOES NOT have any THIS reference
-// const results = numbers.map(number => {
-//   return number + 1;
-// });
+const jackfruitIndex = fruits.indexOf("jackfruit");
+const guavaIndex = fruits.indexOf("guava");
 
-strings.forEach(string => {
-  console.log("string is", string);
-});
+console.log(jackfruitIndex, guavaIndex);
 
-function updateNumbersAndStrings(officialArr, stringUpdate, numberUpdate) {
-  return officialArr.map(el => {
-    if (typeof el === "string") {
-      return (el += stringUpdate);
-    }
-    if (typeof el === "number") {
-      return (el += numberUpdate);
-    }
-  });
-}
-console.log(updateNumbersAndStrings(numbersAndStrings, "hello", 236));
-//Map will ALWAYS return an array with matching length and returns something.
+const spliceReturns = fruits.splice(3, 3, "raspberries");
+console.log(spliceReturns);
 
-//   console.log("current element is", el);
-//   if (typeof el === "string") {
-//     updatedNumbersAndStrings.push((el += stringUpdate));
-//   } else {
-//     updatedNumbersAndStrings.push((el += numberUpdate));
-//   }
+fruits.splice(3, 3, "raspberries");
+console.log(fruits.splice);
+// slice(start, end)
+// splits an array into smaller pieces
 
-//   for (let i = 0; i < officialArr.length; i += 1) {
-//     if (typeof officialArr[i] === "string") {
-//       updatedNumbersAndStrings.push((officialArr[i] += stringUpdate));
-//     } else {
-//       updatedNumbersAndStrings.push((officialArr[i] += numberUpdate));
-//     }
-//   }
-//   return updatedNumbersAndStrings;
-// }
+const smallerArray = fruits.slice(jackfruitIndex);
+console.log("fruits is", fruits);
+//console.log('smallerArray is', smallerArray);
+console.log(smallerArray);
+//Includes the start but excludes the ending
+const bestFruits = fruits.slice(jackfruitIndex, guavaIndex + 2);
+console.log("bestFruits are", bestFruits);
 
-// console.log("numbers is", numbers);
-// console.log("strings is", strings);
-// // console.log("arr is", officialArr);
+// includes returns true or false depending on
+// whether or not the argument is in the array
+const mysteryValue = fruits.includes("dragonfruit");
+console.log(mysteryValue);
+
+// indexOf returns the index of the first occurence
+// of our argument in the array
+console.log(fruits.indexOf("pineapple"));
+
+// indexOf returns the index of the last occurence
+// of our argument in the array
+console.log(fruits.lastIndexOf("pineapple"));
