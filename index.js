0c2986027e6996751d730eded5e07f57fc56bdfb
Wallat Baban <WlatM.Baban@gmail.com>
2019-12-05 20:52:26 -0600

    Add reduce example with numbers

--- index.js
+++ index.js
@@ -7,43 +7,20 @@ function add1(n) {
 function double(n) {
   return n * 2;
 }
-function odds(n) {
-  return n % 2;
+
+function add1AndDouble(n) {
+  return double(add1(n));
 }
 
-// function add1AndDouble(n) {
-//   return double(add1(n));
-// }
+function doubleAndAdd1(n) {
+  return add1(double(n));
+}
 
-// function doubleAndAdd1(n) {
-//   return add1(double(n));
-// }
+// const sumOfNums = nums.reduce((total, currentNum) => total + currentNum);
+// console.log(sumOfNums);
 
-// const numsAddedOneAndDoubles = nums.map(num => add1AndDouble(num));
+function getAvg(nums) {
+  return nums.reduce((total, num) => total + num) / nums.length;
+}
 
-//My Solution
-// const add1Thing = nums.map(num => add1(num));
-// const doubleIt = nums.map(num => double(num));
-
-// //proper solution:
-
-// // console.log(nums.map(num => add1(num)).map(num => double(num)));
-
-// // function add1AndDoubleEachNum
-
-// // console.log(numsAddedOneAndDoubles);
-// console.log(add1Thing);
-// console.log(doubleIt);
-
-// const oddNums = nums.filter(num => odds(num));
-
-// console.log(oddNums);
-
-// //proper solution
-
-const results = nums
-  .filter(num => num % 2 === 1)
-  .map(num => add1(num))
-  .map(num => double(num));
-
-console.log(results);
+console.log(getAvg(nums));
